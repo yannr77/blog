0e0f1f479eccb924beb983f2b22edada7a4f96ec
Yann Rochard <yann.rochard@gmail.com>
2012-01-30 14:39:12 +0100

    Remove Method for links admin

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,12 +3,4 @@ module PostsHelper
   def join_tags(post)
     post.tags.map { |t| t.name }.join(", ")
   end
-  
-  def links_for_admin(post)
-    if admin_signed_in?
-      edit = link_to 'Edit', edit_post_path(post)
-      destroy = link_to 'Destroy', post, confirm: 'Are you sure?', method: :delete
-      content_tag(:td, edit) + content_tag(:td, destroy)
-    end
-  end
 end

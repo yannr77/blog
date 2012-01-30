5f507308836e16f4fd3200322e3d31fdb3815aa5
Yann Rochard <yann.rochard@gmail.com>
2012-01-30 10:41:36 +0100

    New method for admin. Recherche

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,5 +3,12 @@ module PostsHelper
   def join_tags(post)
     post.tags.map { |t| t.name }.join(", ")
   end
-
+  
+  def admin_connected?(post)
+    if admin_signed_in?
+      edit = link_to 'Edit', edit_post_path(post)
+      destroy = link_to 'Destroy', post, confirm: 'Are you sure?', method: :delete
+      return edit + " " + destroy
+    end
+  end
 end

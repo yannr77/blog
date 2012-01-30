c50ed47cc66d6dcbbfd7d762f9c58986d18209cd
Yann Rochard <yann.rochard@gmail.com>
2012-01-30 11:19:23 +0100

    Adding method : links for admin.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -4,11 +4,11 @@ module PostsHelper
     post.tags.map { |t| t.name }.join(", ")
   end
   
-  def admin_connected?(post)
+  def links_for_admin(post)
     if admin_signed_in?
       edit = link_to 'Edit', edit_post_path(post)
       destroy = link_to 'Destroy', post, confirm: 'Are you sure?', method: :delete
-      return edit + " " + destroy
+      content_tag(:td, edit) + content_tag(:td, destroy)
     end
   end
 end

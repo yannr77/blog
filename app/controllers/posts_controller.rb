6672531d13940674467df396136c50160c9fef44
Yann Rochard <yann.rochard@gmail.com>
2012-01-31 17:27:50 +0100

    Stylesheets changes + paginate.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,7 @@ class PostsController < ApplicationController
   # GET /posts.json
   def index
     @posts = Post.all
+    @posts = Post.search(params[:search], params[:page])
 
     respond_to do |format|
       format.html # index.html.erb
--- app/models/post.rb
+++ app/models/post.rb
@@ -7,5 +7,12 @@ class Post < ActiveRecord::Base
   
   accepts_nested_attributes_for :tags, :allow_destroy => :true,
     :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } 
-  }  
+  }
+  
+  
+  def self.search(search, page)
+    paginate :per_page => 6, :page => page,
+            :conditions => ['name like ?', "%#{search}%"],
+            :order => 'created_at DESC'
+  end  
 end
